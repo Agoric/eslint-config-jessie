f867a068c75e2240c733cbfcc7b13d4e61627f26
Kate Sills <katelynsills@gmail.com>
2018-11-15 18:32:33 -0800

    add error messages

--- index.js
+++ index.js
@@ -29,23 +29,70 @@ module.exports = {
 		"no-restricted-globals": ["error", "RegExp", "Date"],
 		"no-restricted-syntax": [
 			"error",
-			"BinaryExpression[operator='in']",
-			"BinaryExpression[operator='instanceof']",
-			"NewExpression",
-			"FunctionDeclaration[generator=true]",
-			"FunctionExpression[generator=true]",
-			"FunctionDeclaration[async=true]",
-			"FunctionExpression[async=true]",
-			"ArrowFunctionExpression[async=true]",
-			"DoWhileStatement",
-			"ThisExpression",
-			"UnaryExpression[operator='delete']",
-			"ForInStatement",
-			"MemberExpression[computed=true]",
-			"Super",
-			"MetaProperty",
-			"ClassExpression",
-			"CallExpression[callee.name='eval']",
+			{
+				"selector": "BinaryExpression[operator='in']",
+				"message": "'in' is not allowed."
+			},
+			{
+				"selector": "BinaryExpression[operator='instanceof']",
+				"message": "'instanceof' is not allowed."
+			},
+			{
+				"selector": "NewExpression",
+				"message": "'new' is not allowed."
+			},
+			{
+				"selector": "FunctionDeclaration[generator=true]",
+				"message": "generators are not allowed."
+			},
+			{
+				"selector": "FunctionDeclaration[async=true]",
+				"message": "async functions are not allowed."
+			},
+			{
+				"selector": "FunctionExpression[async=true]",
+				"message": "async functions are not allowed."
+			},
+			{
+				"selector": "ArrowFunctionExpression[async=true]",
+				"message": "async functions are not allowed."
+			},
+			{
+				"selector": "DoWhileStatement",
+				"message": "do while statements are not allowed."
+			},
+			{
+				"selector": "ThisExpression",
+				"message": "'this' not allowed."
+			},
+			{
+				"selector": "UnaryExpression[operator='delete']",
+				"message": "'delete' not allowed."
+			},
+			{
+				"selector": "ForInStatement",
+				"message": "for in statements are not allowed."
+			},
+			{
+				"selector": "MemberExpression[computed=true]",
+				"message": "computed property names are not allowed."
+			},
+			{
+				"selector": "Super",
+				"message": "'super' is not allowed."
+			},
+			{
+				"selector": "MetaProperty",
+				"message": "'MetaProperty' is not allowed."
+			},
+			{
+				"selector": "ClassExpression",
+				"message": "'ClassExpression' is not allowed."
+			},
+			{
+				"selector": "CallExpression[callee.name='eval']",
+				"message": "'ClassExpression' is not allowed."
+			},
 		],
 	}
 };
